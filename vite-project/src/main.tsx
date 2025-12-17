4aec7d2538b301aaf44844069967c5b3dfa1e643
sharonjvj <sharonjadelyn05@gmail.com>
2025-12-17 19:36:28 +0700

    Struktur folder awal

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { RouterProvider } from 'react-router'
+// import router from './routes/router'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <RouterProvider router={router} /> */}
   </StrictMode>,
 )
